8e14e9b9d474cc73935c8372071a49cbdc8a072f
Serhatuzn <seerhatuzun@gmail.com>
2024-11-07 20:00:32 +0300

    Try-Catch

--- Try-Catch/Program.cs
+++ Try-Catch/Program.cs
@@ -3,8 +3,8 @@
     Console.Write("Lütfen 1. sayıyı giriniz : "); // kullanıcan 1. sayıyı girmesini istiyoruz.
     int sayi = Convert.ToInt32(Console.ReadLine());
 
-    Console.Write("Lütfen 2. sayıyı giriniz : "); // kullanıcan 2. sayıyı girmesini istiyoruz.
-    int sayi2 = Convert.ToInt32(Console.ReadLine());
+    int sonuc  = sayi * sayi;
+    Console.Write($"Cevap : {sonuc}");
 }
 
 catch (FormatException) //FortmatException diyerek bir dizeyi dönüştürmeye çalışırken hatalı giriş kullanıldığında.
